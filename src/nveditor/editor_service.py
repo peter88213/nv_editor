09434b5fb36998a52be121d7942c72d489b3c3ad
peter88213 <petert88213@gmail.com>
2026-01-12 13:13:13 +0100

    Refactored the configuration.

--- src/nveditor/editor_service.py
+++ src/nveditor/editor_service.py
@@ -54,12 +54,12 @@ class EditorService(SubController, Observer):
             configDir = f'{homeDir}/{self.INI_FILEPATH}'
         except:
             configDir = '.'
-        self.iniFile = f'{configDir}/{self.INI_FILENAME}'
         self.configuration = self._mdl.nvService.new_configuration(
             settings=self.SETTINGS,
             options=self.OPTIONS,
+            filePath=f'{configDir}/{self.INI_FILENAME}',
         )
-        self.configuration.read(self.iniFile)
+        self.configuration.read()
         self.prefs = {}
         self.prefs.update(self.configuration.settings)
         self.prefs.update(self.configuration.options)
@@ -116,7 +116,7 @@ class EditorService(SubController, Observer):
                 self.configuration.options[keyword] = self.prefs[keyword]
             elif keyword in self.configuration.settings:
                 self.configuration.settings[keyword] = self.prefs[keyword]
-        self.configuration.write(self.iniFile)
+        self.configuration.write()
 
     def open_editor_window(self):
         """Create a section editor window 
