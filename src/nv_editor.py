590843c7d320413febf46a6842f0f8ac6239dfbb
peter88213 <petert88213@gmail.com>
2026-01-12 13:17:35 +0100

    Updated the copyright notice.

--- src/nv_editor.py
+++ src/nv_editor.py
@@ -1,7 +1,7 @@
 """A multi-section "plain text" editor plugin for novelibre.
 
 Requires Python 3.7+
-Copyright (c) 2025 Peter Triesberger
+Copyright (c) Peter Triesberger
 For further information see https://github.com/peter88213/nv_editor
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 
